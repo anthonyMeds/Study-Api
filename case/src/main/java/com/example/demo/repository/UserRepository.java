2a32a82106568d532a7312aca396b1c4f6927ed2
anthonyMeds <anthony.eduardo@hotmail.com>
2024-04-17 23:23:22 -0300

    feat: user respository

---
+++ case/src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.users.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByUsername(String username);
+
+    Optional<User> findUserById(Long id);
+}

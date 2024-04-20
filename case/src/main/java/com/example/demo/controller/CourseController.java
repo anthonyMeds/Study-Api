c2e64a38ba25b79bc2cc727db9c71694c29fbc8a
aempantaleao <anthony.medeiros@fazenda.se.gov.br>
2024-04-20 18:34:06 -0300

    feat : course controller and request body

--- case/src/main/java/com/example/demo/controller/CourseController.java
+++ case/src/main/java/com/example/demo/controller/CourseController.java
@@ -1,2 +1,50 @@
-package com.example.demo.controller;public class CourseController {
+package com.example.demo.controller;
+
+import com.example.demo.domain.courses.Course;
+import com.example.demo.dto.course.CourseDto;
+import com.example.demo.service.CourseService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/course")
+public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    @Operation(summary = "Create a course",
+            description = """
+                    
+                    To create an course it is requested to inform a valid : 
+                    - Name
+                    - Code
+                    - Instructor
+                    - Description
+                    - Status
+                    - Creation Date
+                    
+                    """)
+    @PostMapping("/create")
+    public ResponseEntity<Course> createUser
+            (
+                    @Valid
+                    @RequestBody
+                    CourseDto courseDto
+
+            ) throws Exception {
+
+        Course newCourse = courseService.createCourse(courseDto);
+
+        return new ResponseEntity<>(newCourse, HttpStatus.CREATED);
+
+    }
+
 }
--- case/src/main/java/com/example/demo/dto/course/CourseDto.java
+++ case/src/main/java/com/example/demo/dto/course/CourseDto.java
@@ -1,2 +1,35 @@
-package com.example.demo.dto;public class CourseDto {
+package com.example.demo.dto.course;
+
+import com.example.demo.domain.courses.CourseStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record CourseDto
+        (
+                @Schema(description = "Name of the course", example = "Spring Boot Advanced", required = true)
+                @NotBlank(message = "Name is required")
+                String name,
+
+                @Schema(description = "Code of the course", example = "java-boot", required = true, maxLength = 10)
+                @Pattern(regexp = "^[a-zA-Z-]+$", message = "Code must contain only alphabetic characters and hyphens")
+                @NotBlank(message = "Code is required")
+                @Size(max = 10, message = "Code must be at most 10 characters")
+                String code,
+
+                @Schema(description = "Name of the instructor", example = "Joao", required = true)
+                @NotBlank(message = "Instructor name is required")
+                String instructor,
+
+                @Schema(description = "Description of the course", example = "Advanced concepts in Spring Boot", required = true)
+                @NotBlank(message = "Description is required")
+                String description,
+
+                @Schema(description = "Status of the course", example = "ACTIVE", required = true)
+                @NotNull(message = "Status is required")
+                CourseStatus status
+
+        ) {
 }

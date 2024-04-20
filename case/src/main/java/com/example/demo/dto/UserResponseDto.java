014d0f9b5f7ee16064434b69132ba3acd1927c45
aempantaleao <anthony.medeiros@fazenda.se.gov.br>
2024-04-20 16:54:08 -0300

    feat : get user controller

---
+++ case/src/main/java/com/example/demo/dto/UserResponseDto.java
@@ -0,0 +1,23 @@
+package com.example.demo.dto;
+
+import com.example.demo.domain.users.UserRole;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record UserResponseDto        (
+        @Schema(description = "Name of the user", example = "John Doe", required = true)
+        @NotBlank(message = "Name is required")
+        String name,
+
+        @Schema(description = "Email of the user", example = "john@example.com", required = true, format = "email")
+        @NotBlank(message = "Email is required")
+        @Pattern(regexp = "^(.+)@(.+)$", message = "Invalid email format")
+        String email,
+
+        @Schema(description = "Role of the user", example = "STUDENT", required = true)
+        @NotNull(message = "Role is required")
+        UserRole role
+) {
+}

619fe3ef6d94c563a47c7d0c8ce1a1a63cd0cefa
aempantaleao <anthony.medeiros@fazenda.se.gov.br>
2024-04-20 18:35:36 -0300

    feat : course service

--- case/src/main/java/com/example/demo/service/CourseService.java
+++ case/src/main/java/com/example/demo/service/CourseService.java
@@ -1,2 +1,52 @@
-package com.example.demo.service;public class CourseService {
+package com.example.demo.service;
+
+import com.example.demo.domain.courses.Course;
+import com.example.demo.domain.users.User;
+import com.example.demo.domain.users.UserRole;
+import com.example.demo.dto.course.CourseDto;
+import com.example.demo.repository.CourseRepository;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CourseService {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Course createCourse(CourseDto courseDto) throws Exception {
+
+        Course course = new Course(courseDto);
+
+        validateCourseDoesNotExist(course.getCode());
+
+        validateInstructor(course.getInstructor());
+
+        courseRepository.save(course);
+
+        return course;
+    }
+
+    public void validateCourseDoesNotExist(String code) throws Exception {
+        if (courseRepository.existsByCode(code)) {
+            throw new Exception("Course already exists.");
+        }
+    }
+
+    public void validateInstructor(String instructorUsername) throws Exception {
+        Optional<User> userOptional = userRepository.findUserByUsername(instructorUsername);
+        User instructor = userOptional.orElseThrow(() -> new Exception("Instructor not found."));
+
+        if (!instructor.getRole().equals(UserRole.INSTRUCTOR)) {
+            throw new RuntimeException("Only instructors can create courses.");
+        }
+
+    }
+
 }

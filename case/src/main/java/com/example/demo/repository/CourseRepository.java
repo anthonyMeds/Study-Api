893a076fef47efb57887b3ef2a66d6c371624fdc
aempantaleao <anthony.medeiros@fazenda.se.gov.br>
2024-04-20 18:35:20 -0300

    feat : course repository

--- case/src/main/java/com/example/demo/repository/CourseRepository.java
+++ case/src/main/java/com/example/demo/repository/CourseRepository.java
@@ -1,2 +1,12 @@
-package com.example.demo.repository;public interface CourseRepository {
+package com.example.demo.repository;
+
+import com.example.demo.domain.courses.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    Optional<Course> findByCode(String code);
+    boolean existsByCode(String code);
 }

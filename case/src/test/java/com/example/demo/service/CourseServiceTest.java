10c887ad7c317c23b1ae1ae25986fb006deb8048
aempantaleao <anthony.medeiros@fazenda.se.gov.br>
2024-04-20 21:17:58 -0300

    test : unit test to get a course

--- case/src/test/java/com/example/demo/service/CourseServiceTest.java
+++ case/src/test/java/com/example/demo/service/CourseServiceTest.java
@@ -5,6 +5,7 @@ import com.example.demo.domain.courses.CourseStatus;
 import com.example.demo.domain.users.User;
 import com.example.demo.domain.users.UserRole;
 import com.example.demo.dto.course.CourseDto;
+import com.example.demo.dto.course.ResponseCourseDto;
 import com.example.demo.repository.CourseRepository;
 import com.example.demo.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -14,12 +15,20 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -123,6 +132,42 @@ public class CourseServiceTest {
         assertEquals(courseDto.instructor(), createdCourse.getInstructor());
         assertEquals(courseDto.description(), createdCourse.getDescription());
         assertEquals(courseDto.status(), createdCourse.getStatus());
+    }
+
+    @DisplayName("Test getCourseByStatus method")
+    @Test
+    void testGetCourseByStatus() {
+
+        CourseDto courseDto1 = new CourseDto("Course 1", "code1", "Instructor 1",
+                "Description 1", CourseStatus.ACTIVE);
+        CourseDto courseDto2 = new CourseDto("Course 2", "code2", "Instructor 2",
+                "Description 2", CourseStatus.INACTIVE);
+        List<Course> courses = List.of(new Course(courseDto1), new Course(courseDto2));
+        Page<Course> coursePage = new PageImpl<>(courses);
+
+
+        when(courseRepository.findByStatus(any(), any())).thenReturn(coursePage);
+
+        Page<ResponseCourseDto> response = courseService.getCourseByStatus(CourseStatus.ACTIVE, 0, 10);
+
+        assertEquals(2, response.getTotalElements());
+        assertEquals(2, response.getContent().size());
+        assertEquals("Course 1", response.getContent().get(0).name());
+        assertEquals("code1", response.getContent().get(0).code());
+        assertEquals("Instructor 1", response.getContent().get(0).instructor());
+        assertEquals("Description 1", response.getContent().get(0).description());
+        assertEquals(CourseStatus.ACTIVE, response.getContent().get(0).status());
+        assertNull(response.getContent().get(0).inactivationDate());
+
+        assertEquals("Course 2", response.getContent().get(1).name());
+        assertEquals("code2", response.getContent().get(1).code());
+        assertEquals("Instructor 2", response.getContent().get(1).instructor());
+        assertEquals("Description 2", response.getContent().get(1).description());
+        assertEquals(CourseStatus.INACTIVE, response.getContent().get(1).status());
+        assertNull(response.getContent().get(1).inactivationDate());
+
+        verify(courseRepository, times(1))
+                .findByStatus(CourseStatus.ACTIVE, PageRequest.of(0, 10));
     }
 
 }
